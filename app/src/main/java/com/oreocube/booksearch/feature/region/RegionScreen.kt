649e1604d9eb9504f09088581a80504f08ac6d98
Jihye Joo <oreocube13@gmail.com>
2025-03-11 14:01:36 +0900

    feat: 지역 선택 표 제목 컴포저블 구현

--- app/src/main/java/com/oreocube/booksearch/feature/region/RegionScreen.kt
+++ app/src/main/java/com/oreocube/booksearch/feature/region/RegionScreen.kt
@@ -3,21 +3,27 @@ package com.oreocube.booksearch.feature.region
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.oreocube.booksearch.core.ui.theme.Brown10
 import com.oreocube.booksearch.core.ui.theme.Brown60
+import com.oreocube.booksearch.core.ui.theme.Grey80
 import com.oreocube.booksearch.domain.model.City
 import com.oreocube.booksearch.domain.model.District
 
@@ -28,7 +34,75 @@ fun RegionScreen(
     cities: List<City>,
     districts: List<District>,
 ) {
-    Row(Modifier.fillMaxSize()) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            TableTitleItem(
+                modifier = Modifier.weight(1f),
+                text = "시·도",
+            )
+            VerticalDivider(
+                modifier = Modifier.height(IntrinsicSize.Min),
+            )
+            TableTitleItem(
+                modifier = Modifier.weight(1f),
+                text = "시·군·구",
+            )
+        }
+
+        RegionTable(
+            modifier = Modifier.weight(1f),
+            selectedCityId = selectedCityId,
+            selectedDistrictId = selectedDistrictId,
+            cities = cities,
+            districts = districts,
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .background(
+                    color = Brown10,
+                    shape = RoundedCornerShape(10.dp)
+                )
+                .padding(16.dp),
+        ) {
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = "도서관 찾기",
+                textAlign = TextAlign.Center,
+                color = Color.White,
+            )
+        }
+    }
+}
+
+@Composable
+private fun TableTitleItem(
+    modifier: Modifier = Modifier,
+    text: String,
+) {
+    Box(
+        modifier = modifier
+            .background(color = Grey80)
+            .padding(16.dp),
+    ) {
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = text,
+            color = Color.Black,
+        )
+    }
+}
+
+@Composable
+private fun RegionTable(
+    modifier: Modifier = Modifier,
+    selectedCityId: Int,
+    selectedDistrictId: Int,
+    cities: List<City>,
+    districts: List<District>,
+) {
+    Row(modifier = modifier) {
         LazyColumn(
             modifier = Modifier
                 .weight(1f)
