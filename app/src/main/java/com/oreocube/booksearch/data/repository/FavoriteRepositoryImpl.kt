abb6890eff22b7a19cf68ea33fe5bb8c20497544
Jihye Joo <oreocube13@gmail.com>
2025-03-11 10:36:53 +0900

    feat: FavoriteRepository 구현

--- app/src/main/java/com/oreocube/booksearch/data/repository/FavoriteRepositoryImpl.kt
+++ app/src/main/java/com/oreocube/booksearch/data/repository/FavoriteRepositoryImpl.kt
@@ -1,19 +1,24 @@
 package com.oreocube.booksearch.data.repository
 
+import com.oreocube.booksearch.data.database.FavoriteDao
+import com.oreocube.booksearch.data.response.LibraryShortEntity
+import com.oreocube.booksearch.data.response.toEntity
 import com.oreocube.booksearch.domain.model.LibraryShort
 import com.oreocube.booksearch.domain.repository.FavoriteRepository
 import javax.inject.Inject
 
-class FavoriteRepositoryImpl @Inject constructor() : FavoriteRepository {
+class FavoriteRepositoryImpl @Inject constructor(
+    private val favoriteDao: FavoriteDao,
+) : FavoriteRepository {
     override suspend fun getFavoriteLibraries(): List<LibraryShort> {
-        TODO("Not yet implemented")
+        return favoriteDao.getFavoriteLibraries().map(LibraryShortEntity::toModel)
     }
 
     override suspend fun addFavoriteLibrary(library: LibraryShort) {
-        TODO("Not yet implemented")
+        favoriteDao.addFavoriteLibrary(library.toEntity())
     }
 
     override suspend fun deleteFavoriteLibrary(id: String) {
-        TODO("Not yet implemented")
+        favoriteDao.deleteFavoriteLibrary(id)
     }
 }
