34a3bed77cb46a9878e5ef26f2778e34667405f5
Jihye Joo <oreocube13@gmail.com>
2025-03-18 17:58:30 +0900

    feat: 관심 도서관 empty screen 추가

--- app/src/main/java/com/oreocube/booksearch/feature/favorite/library/FavoriteLibraryScreen.kt
+++ app/src/main/java/com/oreocube/booksearch/feature/favorite/library/FavoriteLibraryScreen.kt
@@ -1,18 +1,25 @@
 package com.oreocube.booksearch.feature.favorite.library
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -24,6 +31,7 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.oreocube.booksearch.R
 import com.oreocube.booksearch.core.ui.component.BookSearchTopBar
 import com.oreocube.booksearch.core.ui.theme.Brown20
+import com.oreocube.booksearch.core.ui.theme.Gray20
 import com.oreocube.booksearch.domain.model.LibraryShort
 
 @Composable
@@ -61,11 +69,17 @@ private fun FavoriteLibraryScreen(
         )
         when (uiState) {
             is FavoriteLibraryUiState.Data -> {
-                FavoriteLibraryList(
-                    modifier = Modifier.fillMaxSize(),
-                    libraries = uiState.libraries,
-                    onStarClick = onStarClick,
-                )
+                if (uiState.libraries.isEmpty()) {
+                    EmptyFavoriteLibraryContent(
+                        onSearchClick = onSearchClick,
+                    )
+                } else {
+                    FavoriteLibraryList(
+                        modifier = Modifier.fillMaxSize(),
+                        libraries = uiState.libraries,
+                        onStarClick = onStarClick,
+                    )
+                }
             }
 
             else -> {}
@@ -74,19 +88,58 @@ private fun FavoriteLibraryScreen(
 }
 
 @Composable
-fun FavoriteLibraryList(
+private fun EmptyFavoriteLibraryContent(
+    onSearchClick: () -> Unit,
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(
+            text = stringResource(R.string.no_favorite_library),
+            style = MaterialTheme.typography.bodyMedium,
+            color = Gray20,
+        )
+        Row(
+            modifier = Modifier
+                .padding(top = 16.dp)
+                .clip(shape = RoundedCornerShape(32.dp))
+                .background(color = Brown20)
+                .clickable(onClick = onSearchClick)
+                .padding(horizontal = 24.dp, vertical = 12.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Icon(
+                tint = Color.White,
+                painter = painterResource(R.drawable.ic_search_24),
+                contentDescription = stringResource(R.string.menu_search_library)
+            )
+            Text(
+                modifier = Modifier.padding(start = 8.dp),
+                text = stringResource(R.string.menu_search_library),
+                style = MaterialTheme.typography.bodyLarge,
+                color = Color.White,
+            )
+        }
+    }
+}
+
+@Composable
+private fun FavoriteLibraryList(
     modifier: Modifier = Modifier,
     libraries: List<LibraryShort>,
     onStarClick: (String) -> Unit,
 ) {
     LazyColumn(modifier = modifier) {
-        libraries.forEach { library ->
-            item(key = library.id) {
-                FavoriteLibraryItem(
-                    library = library,
-                    onStarClick = { onStarClick(library.id) },
-                )
-            }
+        items(
+            items = libraries,
+            key = { library -> library.id },
+        ) { library ->
+            FavoriteLibraryItem(
+                library = library,
+                onStarClick = { onStarClick(library.id) },
+            )
         }
     }
 }
@@ -123,13 +176,25 @@ private fun FavoriteLibraryItem(
 
 @Composable
 @Preview(showBackground = true)
-fun FavoriteLibraryScreenPreview() {
+private fun FavoriteLibraryScreenPreview1() {
     FavoriteLibraryScreen(
         uiState = FavoriteLibraryUiState.Data(
             libraries = listOf(
                 LibraryShort(id = "11", name = "자바도서관"),
                 LibraryShort(id = "12", name = "코틀린도서관"),
             )
+        ),
+        onSearchClick = {},
+        onStarClick = {},
+    )
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun FavoriteLibraryScreenPreview2() {
+    FavoriteLibraryScreen(
+        uiState = FavoriteLibraryUiState.Data(
+            libraries = emptyList()
         ),
         onSearchClick = {},
         onStarClick = {},
