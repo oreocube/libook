edba2bd55d85de3441346f08491b80d1681a53e0
Jihye Joo <oreocube13@gmail.com>
2025-03-20 14:15:36 +0900

    feat: 프로가드 설정

--- app/src/main/java/com/oreocube/booksearch/data/response/CityResponse.kt
+++ app/src/main/java/com/oreocube/booksearch/data/response/CityResponse.kt
@@ -1,7 +1,9 @@
 package com.oreocube.booksearch.data.response
 
+import androidx.annotation.Keep
 import com.oreocube.booksearch.domain.model.City
 
+@Keep
 data class CityResponse(
     val id: Int = 0,
     val name: String = "",
--- app/src/main/java/com/oreocube/booksearch/data/response/DistrictResponse.kt
+++ app/src/main/java/com/oreocube/booksearch/data/response/DistrictResponse.kt
@@ -1,7 +1,9 @@
 package com.oreocube.booksearch.data.response
 
+import androidx.annotation.Keep
 import com.oreocube.booksearch.domain.model.District
 
+@Keep
 data class DistrictResponse(
     val id: Int = 0,
     val city_id: Int = 0,
