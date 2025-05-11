2b815b52d6263495da3586b2aa37cab73fffefa7
Jihye Joo <oreocube13@gmail.com>
2025-05-11 19:10:44 +0900

    add: paging 의존성 추가

--- feature/book/build.gradle.kts
+++ feature/book/build.gradle.kts
@@ -8,6 +8,8 @@ android {
 
 dependencies {
     implementation(libs.kotlinx.collections.immutable)
+    implementation(libs.androidx.paging.compose)
+    implementation(libs.androidx.paging.runtime)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
